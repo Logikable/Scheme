89d319264521056e0e1456d81db0a132c340dcfe
empress-wu <ingridwuusa@gmail.com>
2016-11-16 04:31:27 -0600

    up to 18

--- scheme/questions.scm
+++ scheme/questions.scm
@@ -5,28 +5,49 @@
 
 ; Some utility functions that you may find useful to implement.
 (define (map proc items)
-  'replace-this-line)
+  (cond ((null? items) ())
+        (else (cons (proc (car items)) (map proc (cdr items))))
+  )
+)
 
 (define (cons-all first rests)
-  'replace-this-line)
+  (if (null?  rests) nil
+    (cons (append (cons first nil) (car rests)) (cons-all first (cdr rests)))
+  ; (map (lambda (x) (append first x)) rests)
+))
 
 (define (zip pairs)
-  'replace-this-line)
+  (if (null? pairs) nil
+    (define one (cons-all (lambda (x) (car x)) pairs))
+    (define two (cons-all (lambda (s) (cadr s)) pairs))
+    )
+  (cons one two)
+  ; (list one two)
+)
 
 ;; Problem 17
 ;; Returns a list of two-element lists
 (define (enumerate s)
   ; BEGIN PROBLEM 17
-  'replace-this-line
+  (define (helper s index)
+    (if (null? s) s (cons (cons index(cons (car s) nil)) (helper (cdr s) (+ index 1))))
   )
+  (helper s 0)
+)
   ; END PROBLEM 17
 
 ;; Problem 18
 ;; List all ways to make change for TOTAL with DENOMS
 (define (list-change total denoms)
   ; BEGIN PROBLEM 18
-  'replace-this-line
-  )
+  (cond 
+    ((= total 0) nil)
+    ((null? denoms) nil)
+    ((= (car denoms) total) (cons (cons (car denoms) nil) (list-change total (cdr denoms))))
+    ((< total (car denoms)) (list-change total (cdr denoms)))
+    (else (append (cons-all (car denoms) (list-change (- total (car denoms)) denoms)) (list-change total (cdr denoms))))
+   )
+)
   ; END PROBLEM 18
 
 ;; Problem 19
@@ -43,12 +64,12 @@
 (define (let-to-lambda expr)
   (cond ((atom? expr)
          ; BEGIN PROBLEM 19
-         'replace-this-line
+         expr
          ; END PROBLEM 19
          )
         ((quoted? expr)
          ; BEGIN PROBLEM 19
-         'replace-this-line
+         expr
          ; END PROBLEM 19
          )
         ((or (lambda? expr)
@@ -57,18 +78,22 @@
                (params (cadr expr))
                (body   (cddr expr)))
            ; BEGIN PROBLEM 19
-           'replace-this-line
+
+           (print expr)
+           (print 1)
            ; END PROBLEM 19
            ))
         ((let? expr)
          (let ((values (cadr expr))
                (body   (cddr expr)))
            ; BEGIN PROBLEM 19
-           'replace-this-line
+           (cons (lambda (cons (caar values) (cons (cdar values) nil)) body) (cons (cadr values) (cons (cddr values) nil))) 
+           (print values)
+           (print body)
            ; END PROBLEM 19
            ))
         (else
          ; BEGIN PROBLEM 19
-         'replace-this-line
+         expr
          ; END PROBLEM 19
          )))
